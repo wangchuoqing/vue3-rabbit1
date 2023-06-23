97953cfb0aeec7ac5c901a6c1f323dac5b94daeb
能能 <714298288@qq.com>
2023-06-23 16:10:36 +0800

    layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    首页
-    <!-- 二级路由出口 -->
-    <RouterView />
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
